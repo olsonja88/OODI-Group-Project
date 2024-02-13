a6299d18d85d4535e283c9a79d132d21e8cbf1d4
jadawent <jadawent@gmail.com>
2024-02-12 19:28:09 -0600

    menu class

--- src/main/java/edu/metrostate/model/BasicFoodItem.java
+++ src/main/java/edu/metrostate/model/BasicFoodItem.java
@@ -2,4 +2,57 @@ package edu.metrostate.model;
 
 public class BasicFoodItem {
     // TODO: Fill out this class from Lucid Chart diagram
+    private String name;
+    private static int foodID = 0;
+    private String description;
+    private String category;
+    private float price;
+
+    public BasicFoodItem(String name, String description, String category, float price) {
+        name = name;
+        description = null;
+        category = null;
+        price = 0;
+        foodID = foodID++;
+    }
+
+    public void setName(String name1) {
+        name = name1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setID(int num) {
+        foodID = num;
+    }
+
+    public int getID() {
+        return foodID;
+    }
+
+    public void setDescription(String decs) {
+        description = decs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setCategory(String cat) {
+        category = cat;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setPrice(float f) {
+        price = f;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
