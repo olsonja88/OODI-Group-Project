a0ba02216967f86eddc8d4fc058b72dc3352a5f9
jaolson88 <olsonja88@gmail.com>
2024-02-26 19:13:54 -0600

    fix: Change file structure to help build frontend features.

--- src/main/java/edu/metrostate/MainApp.java
+++ src/main/java/edu/metrostate/MainApp.java
@@ -6,46 +6,17 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.net.URL;
-
 public class MainApp extends Application {
-
-    private final ValueStore store;
-
-    public MainApp() {
-        this.store = new ValueStore();
-    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("scene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
         BorderPane root = loader.load();
 
-        MainSceneController mainSceneController = loader.getController();
-        mainSceneController.setValueStore(store);
-
-        MainToolBar mainToolBar = new MainToolBar();
-        MainToolBarController mainToolBarController = new MainToolBarController(mainToolBar, store);
-        root.setTop(mainToolBar);
-
         Scene scene = new Scene(root);
-
-        loadStylesheetIntoScene(scene);
 
         stage.setTitle("ICS 372 - HelloFX");
         stage.setScene(scene);
         stage.show();
-    }
-
-    private void loadStylesheetIntoScene(Scene scene) {
-        URL stylesheetURL = getClass().getResource("style.css");
-        if (stylesheetURL == null) {
-            return;
-        }
-        String urlString = stylesheetURL.toExternalForm();
-        if (urlString == null) {
-            return;
-        }
-        scene.getStylesheets().add(urlString);
     }
 }
