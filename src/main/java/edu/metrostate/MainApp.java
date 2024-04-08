e23d2eeab5566e2111c4e5045d0c6c828eb177ca
jadawent <jadawent@gmail.com>
2024-04-08 18:58:44 -0500

    main screen now goes to scroll page with click of button scroll page is finsihed

--- src/main/java/edu/metrostate/MainApp.java
+++ src/main/java/edu/metrostate/MainApp.java
@@ -1,22 +1,50 @@
 package edu.metrostate;
 
 import edu.metrostate.ui.LandingPageController;
+import edu.metrostate.ui.RestaurantPageController;
+import edu.metrostate.ui.ScrollPageController;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainApp extends Application {
+
+    @FXML
+    private Parent landingParent;
+
+    @FXML
+    private Parent scrollParent;
+
+    @FXML LandingPageController landingPageController;
+
+    @FXML ScrollPageController scrollPageController;
+
+    @FXML RestaurantPageController restaurantPageController;
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("LandingPage.fxml"));
         VBox root = loader.load();
 
+        RestaurantPageController restaurantPageController = loader.getController();
+
         LandingPageController landingPageController = loader.getController();
 
-        Scene scene = new Scene(root);
+        landingPageController.getSearchButton().setOnAction(event -> {
+            switchToScrollPage(stage);
+        });
+
+        scrollPageController.getMenuOne().setOnAction(event -> {
+            switchToMenuPage(stage);
+        });
+
+  Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("ICS 372 - HelloFX");
         stage.show();
@@ -25,4 +53,30 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    private void switchToScrollPage(Stage stage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ScrollPage.fxml"));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void switchToMenuPage(Stage stage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("RestaurantPage.fxml"));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch( IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/edu/metrostate/ui/LandingPageController.java
+++ src/main/java/edu/metrostate/ui/LandingPageController.java
@@ -1,21 +1,47 @@
 package edu.metrostate.ui;
 
+//import com.sun.javafx.stage.EmbeddedWindow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class LandingPageController {
+
+    public void LandingListener() {
+
+    }
+
 
     @FXML
     private ComboBox<String> comboBox;
 
-    public void initialize()
-    {
+    @FXML
+    private Button searchButton;
+
+    public void initialize() {
         ObservableList<String> categories = FXCollections.observableArrayList(
                 "Category 1", "Category 2", "Category 3", "Category 4"
         );
 
         comboBox.setItems(categories);
     }
+
+    public Button getSearchButton(){
+        return searchButton;
+    }
+
 }
